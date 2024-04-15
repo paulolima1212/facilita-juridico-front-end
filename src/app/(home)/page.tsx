165f24f3c841fc28f2e3991fc1e4b9f6e70b5b71
Paulo Lima <plima12121984@gmail.com>
2024-04-15 20:24:28 -0300

    feat: install shadcn ui

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { Card } from '@/components/card'
+import { Input } from '@/components/ui/input'
 import { getAllShirts } from '@/service/getAllShirts'
 import Link from 'next/link'
 
@@ -7,17 +8,22 @@ export default async function Home() {
 
   return (
     <main className='w-full items-center justify-center min-h-full'>
-      <div className='flex flex-wrap gap-6 w-full'>
-        {shirts.map((shirt) => (
-          <Link href={`/details/${shirt.id}`} key={shirt.id}>
-            <Card
-              image={shirt.image}
-              price={shirt.price}
-              size={shirt.size}
-              title={shirt.title}
-            />
-          </Link>
-        ))}
+      <div className='flex flex-col'>
+        <div className='mb-6'>
+          <Input placeholder='Faça uma busca...' />
+        </div>
+        <div className='flex flex-wrap gap-6 w-full'>
+          {shirts.map((shirt) => (
+            <Link href={`/details/${shirt.id}`} key={shirt.id}>
+              <Card
+                image={shirt.image}
+                price={shirt.price}
+                size={shirt.size}
+                title={shirt.title}
+              />
+            </Link>
+          ))}
+        </div>
       </div>
     </main>
   )
--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -11,10 +11,12 @@ export default async function Details({ params }: { params: { id: string } }) {
   return (
     <main className='flex w-full'>
       <div className='flex flex-row w-full text-zinc-100'>
-        <Image alt='' src={shirt.image} width={800} height={1000} />
-        <p>{shirt.title}</p>
-        <p>{shirt.size}</p>
-        <p>{shirt.price}</p>
+        <Image alt='' src={shirt.image} width={400} height={600} />
+        <div className='flex flex-col gap-6 ml-6'>
+          <p className='text-4xl'>{shirt.title}</p>
+          <p className='text-2xl'>{shirt.size}</p>
+          <p className='text-4xl'>R$ {shirt.price}</p>
+        </div>
       </div>
     </main>
   )
